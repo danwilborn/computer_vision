626bf9509e93e1d72f943a802e3c9ec8e0a75cc8
Catherine Badart <cbadart@DESKTOP-0V8P6TL.localdomain>
2017-12-06 05:42:02 -0500

    working on building query image

---
+++ combine_images.py
@@ -0,0 +1,66 @@
+import os, sys
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import glob
+
+# get all the training images
+# store in img_dict where key is what digit it is and value is list of all the image files
+img_dict = dict()
+for subdir, dirs, files in os.walk('imgs\\training'):
+	for i in range(10):
+		num_dir = os.path.join(subdir, str(i))
+		for sub, dir, imgs in os.walk(num_dir):
+			for img in imgs:
+				if i not in img_dict:
+					img_dict[i] = list()
+				file_path = os.path.join(num_dir, img)
+				#print(file_path)
+				img_dict[i].append(file_path)
+for num in img_dict:
+	print(num)
+	for i, img in enumerate(img_dict[num]):
+		if i < 10:
+			print(img)
+		else:
+			continue
+			
+# generate 50 row images with 100 images per row
+# each digit has 500 training images so there are 5 rows of 100 per digit, 50 total
+total_rows = 0
+row_size = 20
+for num in img_dict:
+	num_rows = 0
+	while num_rows < 5:
+		#print('building row {} of number {}'.format(str(num_rows), str(num)))
+		# base picture
+		base = cv2.imread(img_dict[num][row_size*num_rows])
+		himstack = cv2.resize(base, None, fx = 2, fy = 2, interpolation = cv2.INTER_CUBIC)
+		img_count = 1
+		while img_count < row_size:
+			# calculate index of img file in img_dict[num]
+			index = row_size * num_rows + img_count
+			img = img_dict[num][index]
+			im = cv2.imread(img)
+			im = cv2.resize(base, None, fx = 2, fy = 2, interpolation = cv2.INTER_CUBIC)
+			himstack = np.hstack((himstack, im))
+			img_count += 1
+		num_rows += 1
+		cv2.imwrite("row-" + str(total_rows) + ".png", himstack)
+		total_rows += 1
+		
+# combine all 50 row-#.png files into one image, stacked horizontaly
+# use glob to get list of all row image files
+os.chdir('.')
+row_imgs = [i for i in glob.glob('row*.png')]
+# base picture
+base = cv2.imread(row_imgs[0])
+imstack = base
+# find a way to sort file names
+for i,img in enumerate(row_imgs):
+	print(img)
+	if i == 0: continue
+	im = cv2.imread(img)
+	imstack = np.vstack((imstack, im))
+# write final query.png
+cv2.imwrite("query.png", imstack)
---
+++ guess_digit.py
@@ -0,0 +1,122 @@
+import os, sys
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from corners import FAST
+
+# Initiate feature detectors
+orb = cv2.ORB_create()
+brisk = cv2.BRISK_create()
+akaze = cv2.AKAZE_create()
+
+target_file = 'imgs/training/4/6.png'
+query_file = 'query.png'
+		
+target_img = cv2.imread(target_file)
+query_img = cv2.imread(query_file)
+
+# resize target to make it easier to get feature descriptors, query is already appropriate size
+target_img = cv2.resize(target_img, None, fx = 2, fy = 2, interpolation = cv2.INTER_CUBIC)
+
+# get keypoints using FAST corner detector
+fast_target = FAST(target_img)
+fast_query = FAST(query_img)
+
+target_kp = fast_target.kp
+query_kp = fast_query.kp
+
+# fast1.show_result()
+# fast2.show_result()
+
+print('######### FAST ##########')
+print('----- Key Points -----')
+print(target_kp)
+print(query_kp)
+print(' ')
+
+# use BRISK with keypoints from FAST to generate descriptors
+target_brisk_kp, target_brisk_desc = brisk.compute(target_img, target_kp)
+query_brisk_kp, query_brisk_desc = brisk.compute(query_img, query_kp)
+
+print('######## BRISK #########')
+print('----- Key Points -----')
+print(target_brisk_kp)
+print(query_brisk_kp)
+print(' ')
+
+print('----- Descriptors -----')
+print(target_brisk_desc)
+print(query_brisk_desc)
+print(' ')
+
+############## Brute Force Matching ##############
+# create BFMatcher object
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+
+# Match descriptors
+# matches = bf.match(BRISKdesc1, BRISKdesc2)
+
+# Sort matches in order of their distance
+# matches = sorted(matches, key = lambda x:x.distance)
+# print(matches)
+
+# Draw first 3 matches
+# img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches[:3],None,flags=2)
+
+# plt.imshow(img3),plt.show()
+
+############## KNN Matching ##############
+# create BFMatcher object
+# bf = cv2.BFMatcher()
+
+# Match descriptors
+# matches = bf.knnMatch(BRISKdesc1, BRISKdesc2, k=2)
+
+# Apply ratio test
+# good = []
+# for m,n in matches:
+	# print('m',m.distance)
+	# print('n',n.distance)
+	# if m.distance < n.distance * 0.8:
+		# good.append(m)
+# print(good)
+
+# Draw matches
+# img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
+
+# plt.imshow(img3),plt.show()
+
+############## FLANN Matching ##############
+FLANN_INDEX_LSH = 6
+index_params = dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 6, # 12
+                   key_size = 12,     # 20
+                   multi_probe_level = 1) #2
+search_params = dict(checks=50)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(target_brisk_desc, query_brisk_desc, k=2)
+print(matches)
+
+good = []
+print("finding good matches:")
+for match in matches:
+	if len(match) == 2:
+		print(match)
+		good.append(match)
+	#print(match, len(match))
+print("matches after cleaning:")
+print(good)	
+matchesMask = [[0,0] for i in range(len(good))]
+# ratio test as per Lowe's paper
+for i,(m,n) in enumerate(good):
+    if m.distance < 0.7*n.distance:
+        matchesMask[i]=[1,0]
+
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = 0)
+
+#img3 = cv2.drawMatchesKnn(target_img,target_brisk_kp,query_img,query_brisk_kp,good,None,**draw_params)
+
+plt.imshow(img3),plt.show()
